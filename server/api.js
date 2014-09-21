804047c10275f41b49522b3bd0de9fed12f22249
seanrose <seanrose@alumni.stanford.edu>
2014-09-21 14:23:59 -0700

    not sure tbh

--- server/api.js
+++ server/api.js
@@ -1,33 +1,44 @@
 /**
  * Given a Twitter userId, creates a following entry
  * for every userId this user follows
- * @param  {String} userId  The Twitter user ID
- * @param  {Array} friends An array of Twitter user objects
+ * @param  {String} userId  A Twitter user ID
+ * @param  {Array} friends An array of Twitter User IDs
  * @return
  */
-function populateRelationships(userId, friends) {
+function populateRelationships(userId, friendIds) {
     console.log('##########\nPopulating for: ' + userId +'\n##########\n');
-    _.each(friends, function(friend) {
+    _.each(friendIds, function(friendId) {
         Followings.upsert(
-            { follow: friend.id_str, followedBy: userId },
-            { $set: { follow: friend.id_str, followedBy: userId } }
+            { follow: friendId, followedBy: userId },
+            { $set: { follow: friendId, followedBy: userId } }
         );
     });
 }
 
-function populateFriendRelationships(userId, friends, twitter) {
-    _.each(friends, function(friend, index) {
-        var friendId = friend.id_str;
-
-
-        var delay = (index + 1) * 65000; // Have to do these in 1 minute intervals because fuck the Twitter API
+/**
+ * Calls populateRelationships on every one
+ * of a user's friends
+ * @param  {String} userId    A Twitter user ID
+ * @param  {Array} friendIds An array of Twitter User IDs
+ * @param  {Object} twitter   An authenticated Twitter api object
+ * @return
+ */
+function populateFriendRelationships(userId, friendIds, twitter) {
+    _.each(friendIds, function(friendId, index) {
+        var delay = (index + 1) * 61000; // Have to do these in 1 minute intervals because fuck the Twitter API
         Meteor.setTimeout(function() {
-            var friendsOfFriends = twitter.friends.list( {id: friendId, count:200} ).data.users;
-            populateRelationships(friendId, friendsOfFriends);
+            var friendsOfFriendIds = twitter.friends.ids( {id: friendId, stringify_ids: true} ).data.ids;
+            populateRelationships(friendId, friendsOfFriendIds);
         }, delay);
     });
 }
 
+/**
+ * Scores the total second degree follows for
+ * a given user
+ * @param  {String} userId A Twitter User ID
+ * @return {Array}        A sorted array
+ */
 function scoreRelationships(userId) {
     var follows = Followings.find({followedBy: userId}, {
         fields: {follow: 1}
@@ -61,13 +72,15 @@ Meteor.methods({
     importFollowings: function() {
         var twitter = new Twitter();
         // TODO(seanrose): handle pagination somehow
-        var friends = twitter.friends.list( {count: 200} ).data.users;
+        var friendIds = twitter.friends.ids( {stringify_ids: true} ).data.ids;
         var userId = Meteor.user().services.twitter.id;
 
         // Populate relationships for the logged in user
-        populateRelationships(userId, friends);
+        populateRelationships(userId, friendIds);
         // Populate relationships for the logged in user's follows
-        populateFriendRelationships(userId, friends, twitter);
+        populateFriendRelationships(userId, friendIds, twitter);
 
     }
 });
+
+console.log(scoreRelationships('17797951'));
